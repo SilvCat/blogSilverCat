bfb3567b45dae48a190e24c66522da4bd41a74dd
SilverCat <1459756077@qq.com>
2023-11-16 15:38:30 +0800

    前端项目上传

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/26/Vue3档案袋a/","2023/11/14/git/","2023/10/24/原ae神核爆/","2023/10/31/文章/","2023/11/02/JQuery/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/11/02/JQuery/","2023/10/26/Vue3档案袋a/","2023/10/24/原ae神核爆/","2023/11/14/git/","2023/10/31/文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
